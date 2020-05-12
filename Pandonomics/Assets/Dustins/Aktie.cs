2fa3acd0a368aa54110dc81b615a27c32c742074
Dustin <dustin.terry@web.de>
2020-05-12 12:47:51 +0200

    test12
    aaaa

---
+++ Pandonomics/Assets/Dustins/Aktie.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+[CreateAssetMenu]
+public class Aktie : ScriptableObject
+{
+    public string stockName;
+    private int gameTime = 13; //total gameDuration in Weeks 
+    public float[] allStockValues; //manually inserted
+
+    public void Awake()
+    {
+        allStockValues = new float[gameTime];
+        for(float f = 0; f < gameTime; f++)
+        {
+            int i = (int)f;
+            if (f < gameTime / 3)
+            {
+                allStockValues[i] = Random.Range(10f, 11f);
+            }
+            else if (f < gameTime / 1.5f)
+            {
+                allStockValues[i] = Random.Range(6f, 7f);
+            }
+            else if (f < gameTime)
+            {
+                allStockValues[i] = Random.Range(8f, 8.5f);
+
+            }
+        }
+    }
+}
---
+++ Pandonomics/Assets/Dustins/AktienGruppe.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+[CreateAssetMenu]
+
+public class AktienGruppe : ScriptableObject
+{
+    private int gameTime = 13; //total gameDuration in Weeks anmerkung an mich: hol die variable vom gamemanager
+    public string stockGroupName; //Obvious
+    public float[] allGrowthValues; //calculated. index 0 is from start of game to week 1
+    public int numberOfStocks; //number of Stocks in the Group
+    public Aktie[] allStocks; //arrayofallstocks in the group
+    public void Awake()
+    {
+        allGrowthValues = new float[gameTime-1];
+    }
+    public void calculateGrowth()
+    {
+        
+        numberOfStocks = allStocks.Length;
+        float[] tmpStockGrowth = new float[numberOfStocks];
+        float sumOfAllStockGrowths;
+        for(int i = 0; i < gameTime - 1; i++)
+        {
+            
+            sumOfAllStockGrowths = 0;
+            for (int j = 0; j < numberOfStocks; j++)
+            {
+
+                tmpStockGrowth[j] = allStocks[j].allStockValues[i + 1] / allStocks[j].allStockValues[i];
+
+            }
+            for(int j = 0; j < numberOfStocks; j++)
+            {
+                sumOfAllStockGrowths += tmpStockGrowth[j];
+            }
+
+            allGrowthValues[i] = (sumOfAllStockGrowths / numberOfStocks)-1;
+        }
+
+    }
+}
