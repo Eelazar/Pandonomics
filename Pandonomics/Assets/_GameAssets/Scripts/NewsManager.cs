6ae6faa8ae4fb9d119da15d2d9f61eacaecd4ffe
Eelazar <maurice.matz@outlook.com>
2020-05-14 16:49:19 +0200

    Added New Newspaper + implementation

---
+++ Pandonomics/Assets/_GameAssets/Scripts/NewsManager.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using TMPro;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class NewsManager : MonoBehaviour
+{
+    public TMP_Text newsArticle_Text;
+
+    public Button breakingNews;
+    public Button tipOfTheDay;
+    public Button moreNews;
+    public Button forecast;
+
+    void Start()
+    {
+        breakingNews.onClick.AddListener(LoadBreakingNews);
+        tipOfTheDay.onClick.AddListener(LoadTipOfTheDay);
+        moreNews.onClick.AddListener(LoadMoreNews);
+        forecast.onClick.AddListener(LoadForecast);
+    }
+
+    private void LoadBreakingNews()
+    {
+        newsArticle_Text.text = "This is the breaking news section";
+    }
+
+    private void LoadTipOfTheDay()
+    {
+        newsArticle_Text.text = "This is the tip of the day section";
+    }
+
+    private void LoadMoreNews()
+    {
+        newsArticle_Text.text = "This is the more news section";
+    }
+
+    private void LoadForecast()
+    {
+        newsArticle_Text.text = "This is the forecast section";
+    }
+}
